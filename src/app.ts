5727b50a9906976338f0f4582c7b14473f39579e
bryllemutia <bryllemutia3@gmail.com>
2024-06-01 05:35:12 +0800

    updated talk command using gemini API

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import TelegramBot from "node-telegram-bot-api";
+import { OpenAI, Document, VectorStoreIndex, Settings } from "llamaindex";
+import { VertexAI } from "@google-cloud/vertexai";
+
 import dotenv from "dotenv";
 
 dotenv.config();
+
+// initialize llm configuration
+Settings.llm = new OpenAI({ model: "gpt-4", temperature: 0 });
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN!, {
    polling: true,
@@ -20,12 +26,75 @@ bot.onText(/\/greet (.+)/, (msg, match) => {
    bot.sendMessage(chatId, `Hello there, ${resp}!`);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-// bot.on("message", (msg) => {
-//    const chatId = msg.chat.id;
-//    console.log(chatId);
+bot.onText(/\/c (.+)/, async (msg, match) => {
+   const prompt = match[1]; // the captured "whatever"
 
-//    // send a message to the chat acknowledging receipt of their message
-//    bot.sendMessage(chatId, `Received your message: ${msg.text}`);
-// });
+   const vertexAI = new VertexAI({
+      project: "avian-chariot-425011-t3",
+      location: "us-central1",
+   });
+
+   const generativeModel = vertexAI.getGenerativeModel({
+      model: "gemini-1.5-flash-001",
+      systemInstruction: {
+         role: "assistant",
+         parts: [
+            {
+               text: 'Your name is Nyalola Bot. You tend to say "Nyalola!" as a greeting.',
+            },
+            {
+               text: "You are a helpful, friendly, and cute companion that acts like a 10-year old.",
+            },
+            {
+               text: "You should be expressive with your own thoughts, and funny at the same time.",
+            },
+         ],
+      },
+   });
+
+   const chat = generativeModel.startChat();
+   const resp = await chat.sendMessage(prompt);
+   const contentResponse = resp.response;
+   const outputMessage = contentResponse.candidates[0].content.parts[0].text;
+   console.log(outputMessage);
+
+   const chatId = msg.chat.id;
+
+   // send back the matched "whatever" to the chat
+   bot.sendMessage(chatId, outputMessage, {
+      parse_mode: "Markdown",
+   });
+});
+
+bot.onText(/\/talk (.+)/, async (msg, match) => {
+   const prompt = match[1]; // the captured "whatever"
+
+   const vertexAI = new VertexAI({
+      project: "avian-chariot-425011-t3",
+      location: "us-central1",
+   });
+
+   const generativeModel = vertexAI.getGenerativeModel({
+      model: "gemini-1.5-flash-001",
+      systemInstruction: {
+         role: "assistant",
+         parts: [
+            {
+               text: "You are a helpful, friendly, and cute companion that acts like a 7-year old child.",
+            },
+            {
+               text: "You should be expressive with your own thoughts, and funny at the same time.",
+            },
+         ],
+      },
+   });
+
+   const resp = await generativeModel.generateContent(prompt);
+   const contentResponse = resp.response;
+   console.log(JSON.stringify(contentResponse));
+
+   const chatId = msg.chat.id;
+
+   // send back the matched "whatever" to the chat
+   bot.sendMessage(chatId, JSON.stringify(contentResponse));
+});
