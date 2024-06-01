868dbaceaae2ca6b86371d590803b1526071addf
bryllemutia <bryllemutia3@gmail.com>
2024-06-01 19:35:52 +0800

    changed model to gemini 1.0 pro

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ const initializeApp = () => {
       });
 
       const generativeModel = vertexAI.getGenerativeModel({
-         model: "gemini-1.5-flash-001",
+         model: "gemini-1.0-pro",
          systemInstruction: {
             role: "assistant",
             parts: [
